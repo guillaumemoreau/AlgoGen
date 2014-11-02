95be4bde336908b9e2c355d79fe65c88d61bb0ae
Guillaume Moreau <guillaume.moreau@ec-nantes.fr>
2014-11-02 17:10:46 +0100

    Initial Commit

---
+++ AlgoGen/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AlgoGen
+//
+//  Created by Guillaume Moreau on 02/11/2014.
+//  Copyright (c) 2014 Guillaume Moreau. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
